1d3038e442b498c71bd94c9f23f96fdf605808d6
your name <37942959+Sardor979bek@users.noreply.github.com>
2023-12-16 18:01:26 -0600

    CS 516 Setup Codebase

--- frontend/src/main/java/com/example/demo/dto/PaymentInfoDto.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.dto;
-
-public record PaymentInfoDto(
-        String cardNumber,
-        String type,
-        String expirationDate,
-        String code) {
-
-}
--- frontend/src/main/java/com/example/demo/dto/ReviewDto.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.dto;
-
-public record ReviewDto(
-                String id,
-                String name,
-                String rate,
-                String review) {
-
-}
